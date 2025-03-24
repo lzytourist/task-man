07168fd2bee13a5d417a3625e45f834ed36be5d2
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-03-24 19:33:31 +0600

    Create and list users

---
+++ account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import (
+    UserListCreateAPIView,
+)
+
+urlpatterns = [
+    path('users/', UserListCreateAPIView.as_view(), name='user-list-create'),
+]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+from account.models import User
+from account.permissions import HasPermission
+from account.serializers import UserSerializer
+
+
+class UserListCreateAPIView(ListCreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated, HasPermission)
+    required_permissions = ['create_user', 'view_user']
