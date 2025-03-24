35181465c5583a6b7fe95adbbab1d1a59fd76fb4
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-03-25 00:48:00 +0600

    JWT Token URLs

--- account/urls.py
+++ account/urls.py
@@ -9,5 +9,7 @@ from .views import (
 )
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/', UserListCreateAPIView.as_view(), name='user-list-create'),
 ]
