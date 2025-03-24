ea87f5b01b7d92f1ebbc63c27b8b52a7e91b4bd9
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-03-24 23:58:08 +0600

    Remove unnecessary condition

--- account/permissions.py
+++ account/permissions.py
@@ -3,10 +3,8 @@ from rest_framework.permissions import BasePermission
 
 class HasPermission(BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            required_permissions = getattr(view, 'required_permissions', None)
-            if required_permissions:
-                permissions = request.user.role.permissions.value_list('codename', flat=True)
-                return all(required_permissions in permission for permission in permissions)
-            return True
-        return False
+        required_permissions = getattr(view, 'required_permissions', None)
+        if required_permissions:
+            permissions = request.user.role.permissions.value_list('codename', flat=True)
+            return all(required_permissions in permission for permission in permissions)
+        return True
