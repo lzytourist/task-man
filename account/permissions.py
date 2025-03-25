29f96c76bb4d7a60ba61ad77271d35c4dc333076
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-03-25 10:57:37 +0600

    Fix: method name

--- account/permissions.py
+++ account/permissions.py
@@ -5,6 +5,6 @@ class HasPermission(BasePermission):
     def has_permission(self, request, view):
         required_permissions = getattr(view, 'required_permissions', None)
         if required_permissions:
-            permissions = request.user.role.permissions.value_list('codename', flat=True)
+            permissions = request.user.role.permissions.values_list('codename', flat=True)
             return all(permission in permissions for permission in required_permissions)
         raise ValueError('required_permissions should be defined HasPermission')
