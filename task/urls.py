1fdf720a71ee3e2bf10b55ce184ae62bb0163c67
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-03-27 15:20:13 +0600

    Assigned task

--- task/urls.py
+++ task/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 
 from .views import (
     TaskListCreateAPIView,
-    TaskRetrieveUpdateDestroyAPIView
+    TaskRetrieveUpdateDestroyAPIView,
+    AssignedTaskListAPIView,
+    AcceptTaskAPIView
 )
 
 urlpatterns = [
     path('', TaskListCreateAPIView.as_view(), name='task-list-create'),
     path('<int:pk>/', TaskRetrieveUpdateDestroyAPIView.as_view(), name='task-retrieve-update-delete'),
+    path('assigned/', AssignedTaskListAPIView.as_view(), name='assigned-task-list'),
+    path('assigned/<int:pk>/accept/', AcceptTaskAPIView.as_view(), name='task-accept'),
 ]
--- task/views.py
+++ task/views.py
@@ -1,5 +1,8 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from account.permissions import HasPermission
 from .models import Task
@@ -30,3 +33,32 @@ class TaskRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
         elif self.request.method == 'DELETE':
             self.required_permissions = ['delete_task']
         return super().get_permissions()
+
+
+class AssignedTaskListAPIView(ListAPIView):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+    permission_classes = [IsAuthenticated, HasPermission]
+    required_permissions = ['view_task']
+
+    def get_queryset(self):
+        return super().get_queryset().filter(assigned_to=self.request.user)
+
+
+class AcceptTaskAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            task_id = kwargs.get('pk')
+
+            task = Task.objects.filter(assigned_to=request.user).get(id=task_id)
+            task.status = Task.Status.ACCEPTED
+            task.save()
+
+            return Response({'message': 'Task accepted'})
+        except Task.DoesNotExist:
+            return Response(
+                data={'message': 'Task does not exists'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
