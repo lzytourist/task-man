b474ebefc883979b45358fe0ad4c72078986ebe4
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-03-27 14:30:09 +0600

    Validate assigned_to

--- task/serializers.py
+++ task/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from account.models import User
 from account.serializers import UserSerializer
 from .models import Task
 
@@ -8,6 +9,11 @@ class TaskSerializer(serializers.ModelSerializer):
     created_by = UserSerializer(read_only=True)
     assigned_to = UserSerializer(read_only=True)
     assigned_to_id = serializers.IntegerField(write_only=True, required=False)
+
+    def validate(self, attrs):
+        if attrs.get('assigned_to_id') and not User.objects.filter(id=attrs.get('assigned_to_id')).exists():
+            raise serializers.ValidationError({'assigned_to_id': ['This user does not exist']})
+        return attrs
 
     def create(self, validated_data):
         validated_data['created_by'] = self.context['request'].user
